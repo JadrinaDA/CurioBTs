bce1c3015dcc42889cbd9a169776a729fc9d4fe0
JadrinaDA <jadrinadeandrade@uc.cl>
2023-07-17 13:44:03 +0100

    Continued documenting and added another file

--- mybehaviors.py
+++ mybehaviors.py
@@ -85,17 +85,30 @@ class ChangePath(py_trees.behaviour.Behaviour):
 class GoHome(py_trees.behaviour.Behaviour):
     """
     Behavior causes the robot to change the path to go home.
+
+    Args:
+        name (:obj:`str`): name of the behaviour
+
+    Attributes:
+        publisher (:obj:`Publisher`) : ROS publisher that alerts movebase node to switch paths
+        blackboard (:obj:`Blackboard`): Blackboard for behavior tree communication
+        curr_path (:obj:`int`): id of the current path
+    
     """
     def __init__(self, name):
         super(GoHome, self).__init__(name=name)
 
     def setup(self, timeout):
+        # Set up the publisher that will send command to switch path
         self.publisher = rospy.Publisher("move_base/switch_path", std_msgs.String, queue_size=10, latch=True)
+        # Set up blackboard to communicate with other nodes
         self.blackboard = py_trees.blackboard.Blackboard()
+        # Home path is always the first
         self.curr_path = 0
         return True
 
     def update(self):
+        # Current path is now going home
         self.blackboard.curr_path = self.curr_path
         self.publisher.publish(std_msgs.String(str(self.curr_path)))
         return py_trees.common.Status.SUCCESS
@@ -103,16 +116,28 @@ class GoHome(py_trees.behaviour.Behaviour):
 class FollowPath(py_trees.behaviour.Behaviour):
     """
     Behavior keeps the robot following the path.
+
+    Args:
+        name (:obj:`str`): name of the behaviour
+
+    Attributes:
+        blackboard (:obj:`Blackboard`): Blackboard for behavior tree communication
+    
+
     """
     def __init__(self, name):
         super(FollowPath, self).__init__(name=name)
 
     def setup(self, timeout):
+        # Set up blackboard to communicate with other nodes
         self.blackboard = py_trees.blackboard.Blackboard()
+        # Start with a pending state
         self.blackboard.state = GoalStatus.PENDING
         return True
 
     def update(self):
+        # Based on the current state I have succeeded, failed or am currently
+        # following the path
         state = int(self.blackboard.state)
         if state == GoalStatus.SUCCEEDED:
             return py_trees.common.Status.SUCCESS
@@ -123,31 +148,53 @@ class FollowPath(py_trees.behaviour.Behaviour):
             return py_trees.common.Status.FAILURE
         
     def terminate(self, new_status):
+        # If I stopped when my goal was active
+        # there was an interruption
         state = int(self.blackboard.state)
         if state == GoalStatus.ACTIVE:
             rospy.loginfo("Goal was preempted")
 
 class Pause(py_trees.behaviour.Behaviour):
     """
-    Behavior causes the robot to pause.
+    Behavior causes the robot to pause. It will resume the previous
+    path when unpaused.
+
+    Args:
+        name (:obj:`str`): name of the behaviour
+
+    Attributes:
+        publisher (:obj:`Publisher`) : ROS publisher that alerts movebase node to pause
+        blackboard (:obj:`Blackboard`): Blackboard for behavior tree communication
+        curr_path (:obj:`int`): id of the current path
+        paused (:obj:`boolean`): if we have paused or not
+    
+
     """
     def __init__(self, name):
         super(Pause, self).__init__(name=name)
 
     def setup(self, timeout):
+        # Set up publisher
         self.publisher = rospy.Publisher("move_base/switch_path", std_msgs.String, queue_size=10, latch=True)
+        # This will let the movebase client know to pause
         self.curr_path = -1
+        # To save the current path when we pause we use the blackboard
         self.blackboard = py_trees.blackboard.Blackboard()
+        # To know if we have paused or not we use a blackboard var
         self.blackboard.pause = False
         self.paused = False
         return True
     
     def initialise(self):
+        # At the start we have not send the goal
         self.goal_sent = False
 
     def update(self):
+        # When ticked if not paused it fails
         if not self.blackboard.pause:
             return py_trees.common.Status.FAILURE
+        # If the pause signal has not been send yet then send it
+        # After sending toggle the booleans
         if not self.goal_sent:
             if not self.paused:
                 self.publisher.publish(std_msgs.String(str(self.curr_path)))
--- obstaclesdist.py
+++ obstaclesdist.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python
+
+##############################################################################
+# Documentation
+##############################################################################
+"""
+ROS node that listens for obstacle information and the position of the robot
+and then publishes if there is an obstacle in the path based on the distances
+calculated.
+"""
+##############################################################################
+# Imports
+##############################################################################
+
+
 import rospy
 from std_msgs.msg import Bool
 from obstacle_detector.msg import Obstacles, SegmentObstacle
@@ -6,12 +20,17 @@ from geometry_msgs.msg import PolygonStamped
 from nav_msgs.msg import Path
 import math
 
+# Publisher to signal to behavior tree is path is clear or not
 pub = rospy.Publisher('/pathclear', Bool, queue_size=10)
+# Boolean to toggle
 obstacle_in_front = False
+# Global variable to store robot position
 pos = (0,0)
+# Global variable to store next point on local path
 next = None
 
 
+# Callback function called when obstacle data is received
 def callback(data):
     global obstacle_in_front
     global pos
@@ -19,6 +38,8 @@ def callback(data):
 
     segments = data.segments
     min = 1000
+    # For each segment detected calculate the middle point, then
+    # calculate the distance between the middle point to robot and the angle
     for segment in segments:
         x = (segment.first_point.x + segment.last_point.x) / 2
         y = (segment.first_point.y + segment.last_point.y) / 2
@@ -28,21 +49,41 @@ def callback(data):
         else:
             result = 10
         rospy.loginfo(str(result)+"x")
-        if dist < min and (abs(result) < math.pi /8):
+        rospy.loginfo(dist)
+        # If distance is lower than our current minimum and angle is on path save it
+        if dist < min and (abs(result) < math.pi /3):
+            min = dist
+    circles = data.circles
+    # Same process for circles
+    for circle in circles:
+        x = circle.center.x
+        y = circle.center.y
+        dist = math.sqrt((x-pos[0])**2 + (y-pos[1])**2)
+        if next:
+            result = math.atan2(y - pos[1], x - pos[0]) - math.atan2(next.y - pos[1], next.x - pos[0])
+        else:
+            result = 10
+        rospy.loginfo(str(result)+"x")
+        rospy.loginfo(dist)
+        if dist < min and (abs(result) < math.pi /6):
             min = dist
     obstacle_in_front = False
     
-
-    if min < 1.2:
+    # We set a threshold to see if there is an obstacle in our path
+    if min < 1.5:
         obstacle_in_front = True
     rospy.loginfo(obstacle_in_front)
     
 
 def callbackg(data):
+    # Callback when we receive robot footprint, transform into position
     global pos
-    pos = (data.polygon.points[3].x, data.polygon.points[3].y)
+    x = (data.polygon.points[0].x + data.polygon.points[1].x) / 2
+    y = (data.polygon.points[0].y + data.polygon.points[1].y) / 2
+    pos = (x, y)
 
 def get_next(data):
+    # Callback when we receive path, take the first point on the path
     global next
     next = data.poses[0].pose.position
     
@@ -56,7 +97,7 @@ def listener():
     # run simultaneously.
     rospy.init_node('listener', anonymous=True)
     
-
+    # Set up our three subscribers and their callbacks
     rospy.Subscriber("/raw_obstacles", Obstacles, callback)
     rospy.Subscriber("/move_base/local_costmap/footprint", PolygonStamped, callbackg)
     rospy.Subscriber("/move_base/TrajectoryPlannerROS/local_plan", Path, get_next)
